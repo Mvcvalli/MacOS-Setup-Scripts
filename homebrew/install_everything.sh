aea011dd8b60af120d539cdc506fa484c99c6314
Mason Choat <45158811+Mvcvalli@users.noreply.github.com>
2021-02-27 16:14:49 +1300

    Create install_everything.sh

---
+++ homebrew/install_everything.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+# Install XCode and Commando Line Tools
+xcode-select --install
+
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew update
+brew upgrade
+
+brew install ${brew[@]} #Homebrew App Installer
+brew cask install ${cask[@]} #Casks Installer
+mas install ${mas[@]} #Mac App Store Installer
+npm install ${npm[@]} #Install npm apps
+
+# Apps
+brew install --cask alacritty
+brew install --cask amethyst
+brew install --cask appcleaner
+brew install --cask atom
+brew install --cask brave-browser
+brew install --cask cheatsheet
+brew install --cask cloudflare-warp
+brew install --cask discord
+brew install --cask dozer
+brew install --cask firefox
+brew install --cask freetube
+brew install --cask lbry
+brew install --cask mailspring
+brew install --cask mpv
+brew install --cask onyx
+brew install --cask protonvpn
+brew install --cask quitter
+brew install --cask rectangle
+brew install --cask rocket
+brew install --cask smcfancontrol
+
+# CLI
+brew install git
+brew install zsh
+brew install htop
+brew install gotop
+brew install ranger
+brew install neofetch
+brew install pfetch
+brew install vim
+brew install newsboat
+
+# Installing Atom Packages
+apm install atom-html-preview
+apm install html-template-generator
+apm install atom-beautify
+
+# Installing Amfora
+brew tap makeworld-the-better-one/tap
+brew install amfora
+
+# Installing kjv
+git clone https://github.com/lukesmithxyz/kjv.git
+cd kjv
+sudo make install
+
+# Installing VimPlug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Installing Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Installing powerlevel9k
+$ git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# Fonts
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+cd
+
