fe341a107bfdbbed5ba3ae5b120ee24cc37f5a02
Mason Choat <45158811+Mvcvalli@users.noreply.github.com>
2021-03-29 17:48:37 +1300

    Update Maintenance.sh

--- homebrew/Maintenance.sh
+++ homebrew/Maintenance.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-echo 'Updating Applications...'
-    brew upgrade         
-    brew update 
-    brew cask upgrade 
-    
-echo 'Cleaning caches & directories...'
-    brew cask cleanup > /dev/null 2>&1
-    rm -rfv /Library/Caches/Homebrew/* > /dev/null 2>&1
-    brew tap --repair > /dev/null 2>&1
-    
-    echo 'All done :)'
+brew update
+brew upgrade
+brew cleanup -s
+brew cask cleanup
 
+brew doctor
+brew missing
